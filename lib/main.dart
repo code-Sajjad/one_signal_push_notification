2e7f79981bcd5e41667686a3876aefca322ccced
Sajjad Javed <56497715+code-Sajjad@users.noreply.github.com>
2024-04-13 12:18:42 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/intro_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ECommerceApp',
+      home: IntroScreen(),
+    );
+  }
+}
